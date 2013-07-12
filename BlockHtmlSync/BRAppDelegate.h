7cd407b7f1e6c16b73b3b9a1c9589ffd99e25f33
Shingo Hashimoto <hashimoto@brick-corp.com>
2013-07-12 12:15:27 +0900

    Initial Commit

---
+++ BlockHtmlSync/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  BlockHtmlSync
+//
+//  Created by Shingo Hashimoto on 13/07/12.
+//  Copyright (c) 2013年 Shingo Hashimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
